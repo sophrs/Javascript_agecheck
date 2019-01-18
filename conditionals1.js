394b6808cc9dd5c5aaf47b87e84ee64b368c3653
sophrs <45198009+sophrs@users.noreply.github.com>
2019-01-18 08:44:21 +0000

    Add files via upload

---
+++ conditionals1.js
@@ -0,0 +1,11 @@
+let person = {name: "Daniel", age: 22, occupation: "student"};
+
+function checkAge(){
+    if (person.age >= 20 && person.age <=40) {
+        document.write("true");
+    }
+    else {
+        document.write("false");
+    }
+
+}
